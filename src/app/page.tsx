e7cba7e630718996dab8d07081acda283154b347
Ankit jain <ajainankit2001@gmail.com>
2024-02-23 11:51:43 +0530

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,28 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
 
+import {useChat} from 'ai/react'
+import GPTLogo from "./components/GPTLogo";
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+  const  {messages,input,handleInputChange,handleSubmit} = useChat()
+  return(
+   <div className='pt-4 pb-32'>
+    {messages.map(message => (
+      <div key={message.id} className='border-t border-black/10 ${
+        message.role === "assistant" && "bg-gray-50"}'>
+        <div className='max-w-3xl mx-auto py-6 flex'>
+          {message.role === "assistant" && <GPTLogo />}
+          <span className='ml-3'>{message.content}</span>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
-}
+        </div>
+    ))}
+    <form className="fixed inset-x-0 bottom-10" onSubmit={handleSubmit}>
+<input
+className='max-w-3xl shadow-xl w-fill mx-auto py-8 flex h-10 rounded-md border-input bg-background px-3 text-sm ring-offset-background  '
+placeholder="send a message"
+value={input}
+onChange={handleInputChange}
+/>
+    </form>
+   </div>
+  )
+} 
